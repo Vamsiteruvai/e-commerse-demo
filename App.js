73500e79307bacd4f0d8a5e95c2b2f1229676e45
TERUVAI VAMSI <teruvaivamsi9@gmail.com>
2024-12-24 17:39:36 +0530

    useMemo_hook

--- App.js
+++ App.js
@@ -1,42 +1,65 @@
-import { useEffect, useState } from "react"
-import Products from "./Products"
+import { useEffect, useMemo, useState } from "react"
 import './App.css'
+import { data } from "react-router-dom";
+
 function App(){
-  var [product,updateproduct]=useState([]);
+    let [Timer,setTime]=useState(new Date().toLocaleTimeString());
 
-  console.log(product)
-  useEffect(
-    ()=>{
-      apiFun()
-  },[]
-)
+    let [product,setProducts]=useState([])
+    
+    
+    useEffect(()=>{
+        dataFetch();
+    },[])
 
-  async function apiFun(){
-    let prodApi=await fetch('https://fakestoreapi.com/products');
-    let prodJson=await prodApi.json();
-    updateproduct(prodJson)
-  }
+    async function dataFetch(){
+        let data=await fetch('https://fakestoreapi.in/api/products');
+        let jsonData=await data.json();
+        console.log(jsonData.products)
+        setProducts(jsonData.products)
+    }
+    
+    
+    const [searchText,setText]=useState("");
+    let filteredproduct=useMemo(()=>{
+        return product.filter((f)=>{
+            return f.title.toLowerCase().includes(searchText.toLowerCase())
+        })
+    },[product,searchText])
 
-  if(product.length==0){
+
+    useEffect(()=>{
+        setInterval(()=>{
+            setTime(new Date().toLocaleTimeString());
+        },1000)
+    },[])
+
+    
     return(
-      <h1>Fetching data....</h1>
+        <>
+            <h1>{Timer}</h1>
+            <input value={searchText} onChange={(e)=>{
+                setText(e.target.value)
+            }} /><br/><br/>
+
+            <table >
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>TITLE</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {filteredproduct.map((p) => (
+                        <tr key={p.id}>
+                            <td>{p.id}</td>
+                            <td>{p.title}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
     )
-  }
-  return(
-  <div className="container">
-  {
-  product.map(
-    (p) => (
-      <Products {...p} key={product.id} />
-    )
-  )
-  }
-</div>
-);
 }
+
 export default App
-
-
-
-
-
